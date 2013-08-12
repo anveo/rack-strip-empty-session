616b5760bf3d88f6c8100a30719b84a6b4830817
Brian Racer <bracer@gmail.com>
2013-08-12 14:38:04 -0600

    gem skeleton

---
+++ lib/rack/strip/empty/session.rb
@@ -0,0 +1,11 @@
+require "rack/strip/empty/session/version"
+
+module Rack
+  module Strip
+    module Empty
+      module Session
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/rack/strip/empty/session/version.rb
@@ -0,0 +1,9 @@
+module Rack
+  module Strip
+    module Empty
+      module Session
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
